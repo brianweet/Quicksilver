6880bd18977041bdcf91385937829d36436a5d6d
Brian Weeteling <brianweeteling@gmail.com>
2018-10-19 15:51:27 +0200

    Create container on ACI

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,19 @@
 $ErrorActionPreference = 'Stop';
 
-Write-Host Starting deploy
+Write-Host Pushing image to ACR
 "$env:ACR_PASS" | docker login --username "$env:ACR_USER" --password-stdin quicksilver.azurecr.io
 docker push quicksilver.azurecr.io/quicksilver-appveyor
+Write-Host Deploy to ACI
+az acr login --name quicksilver --username "$env:ACR_USER" --password "$env:ACR_PASS"
+az container create `
+            --resource-group test-aci `
+            --name quicksilver-appveyor `
+            --image quicksilver.azurecr.io/quicksilver-appveyor `
+            --dns-name-label quicksilver-appveyor-dns `
+            --ports 80 443 `
+            --os-type Windows `
+            --cpu 2 `
+            --memory 2.5 `
+            --registry-login-server quicksilver.azurecr.io `
+            --registry-username "$env:ACR_USER" `
+            --registry-password "$env:ACR_PASS"
