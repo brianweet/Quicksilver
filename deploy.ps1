384156e67abbd7e286f4e55584c900d01299c15f
Brian Weeteling <brianweeteling@gmail.com>
2018-10-19 14:51:20 +0200

    Deploy to acr

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,5 @@
 $ErrorActionPreference = 'Stop';
 
 Write-Host Starting deploy
-"$env:DOCKER_PASS" | docker login --username "$env:DOCKER_USER" --password-stdin
-docker push brianweet/quicksilver-appveyor
+"$env:ACR_PASS" | docker login --username "$env:ACR_USER" --password-stdin quicksilver.azurecr.io
+docker push quicksilver.azurecr.io/quicksilver-appveyor
