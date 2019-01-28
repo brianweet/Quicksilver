baf5fd2031eef9b63857722b662a353700d8c1e8
Brian Weeteling <brianweeteling@gmail.com>
2019-01-28 22:40:46 +0100

    Update resources

--- deploy.ps1
+++ deploy.ps1
@@ -5,12 +5,12 @@ az login --service-principal -u "$env:AZURE_SP_USER" -p "$env:AZURE_SP_PASS" --t
 az container create `
             --resource-group test-aci `
             --name quicksilver-appveyor `
-            --image quicksilver.azurecr.io/quicksilver-appveyor `
+            --image quicksilver.azurecr.io/quicksilver-appveyor-build `
             --dns-name-label quicksilver-appveyor-dns `
             --ports 80 443 `
             --os-type Windows `
-            --cpu 2 `
-            --memory 2.5 `
+            --cpu 1 `
+            --memory 2 `
             --registry-login-server quicksilver.azurecr.io `
             --registry-username "$env:ACR_USER" `
             --registry-password "$env:ACR_PASS"
