d1d9d05056f42dceb03e8c66c20564b2a4e7e2ff
Brian Weeteling <brianweet@users.noreply.github.com>
2019-01-28 23:29:33 +0100

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -4,9 +4,9 @@ Write-Host Deploy to ACI
 az login --service-principal -u "$env:AZURE_SP_USER" -p "$env:AZURE_SP_PASS" --tenant "$env:AZURE_SP_TENANT"
 az container create `
             --resource-group test-aci `
-            --name quicksilver-appveyor `
+            --name quicksilver-appveyor-new `
             --image quicksilver.azurecr.io/quicksilver-appveyor-build `
-            --dns-name-label quicksilver-appveyor-dns `
+            --dns-name-label quicksilver-appveyor-new-dns `
             --ports 80 443 `
             --os-type Windows `
             --cpu 1 `
